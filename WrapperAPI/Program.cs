1bf8f0aadf101cb851b9bfa0970d0cff07f0e812
google_ <glchernenko1@gmail.com>
2021-11-17 20:56:04 +0300

    Initial commit

---
+++ WrapperAPI/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using RestSharp.Serialization.Json;
+
+
+namespace WrapperAPI
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var wrapper = new Wrapper();
+            // var tmp = wrapper.Login("Tester33", "123123");
+            // Console.WriteLine(tmp);
+            var log =wrapper.Login("Tester33332", "123123");
+            Console.WriteLine(log);
+        }
+    }
+}
---
+++ WrapperAPI/Wrapper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Web.UI.WebControls;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace WrapperAPI
+{
+    public class Wrapper
+    {
+        private RestClient Client;
+
+        public Wrapper()
+        {
+            Client = new RestClient("https://test.loy.am")
+            {
+                Authenticator = new HttpBasicAuthenticator("loyam_test", "0IcbmorPNeuEcywxvaGQzznSd3pIl8BF12hT8eeExuZ2G9XYJH7YHeQh")
+            };
+        }
+
+        public string Login(string username, string password)
+        {
+            var request = new RestRequest("/oauth/token", Method.POST);
+            request.AddParameter("grant_type", "password");
+            request.AddParameter("username", username);
+            request.AddParameter("password", password);
+            return Client.Execute(request).Content;;
+        }
+
+        public string Registrate(string username, string password, string email) 
+        {
+            var request = new RestRequest("/api/users",Method.POST);
+            request.AddHeader("Accept", "application/json");
+            request.AddParameter("username", username);
+            request.AddParameter("password", password);
+            request.AddParameter("email", email);
+            return Client.Execute(request).Content;;
+        }
+        
+
+
+        public string RefreshToken(string refreshToken)
+        {
+            var request = new RestRequest("/oauth/token",Method.POST);
+            request.AddHeader("Accept", "application/json");
+            request.AddParameter("refresh_token", refreshToken);
+            return Client.Execute(request).Content;;
+        }
+        
+        // public string UpdateUser() ?
+        
+        // public ApproveEmail() ? откуда берется хэш
+        
+        // Approve email Again ???
+    }
+}
